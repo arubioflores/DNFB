cddf452e6488d3fd4519510abda2580a136ebddf
arubioflores <55721881+arubioflores@users.noreply.github.com>
2019-12-10 16:11:39 -0700

    Update dbo.v_accounts_per_customer_per_year.sql

--- Create Views/dbo.v_accounts_per_customer_per_year.sql
+++ Create Views/dbo.v_accounts_per_customer_per_year.sql
@@ -1,3 +1,33 @@
+/*****************************************************************************************************************
+NAME:    dbo.v_accounts_per_customer_per_year.sql
+PURPOSE: Create the dbo.v_accounts_per_customer_per_year view
+
+SUPPORT: Alex Rubio 
+         rubioalejandro1114@gmail.com
+         aar43@ldsbc.edu
+
+MODIFICATION LOG:
+Ver       Date         Author       Description
+-------   ----------   ----------   -----------------------------------------------------------------------------
+1.0       12/10/2019   ARUBIO      1. Built this view
+1.1       12/10/2019   ARUBIO      2. Added correct formatting
+
+
+
+RUNTIME: 
+1 min
+
+NOTES: 
+(...)
+
+LICENSE: 
+This code is covered by the GNU General Public License which guarantees end users
+the freedom to run, study, share, and modify the code. This license grants the recipients
+of the code the rights of the Free Software Definition. All derivative work can only be
+distributed under the same license terms.
+
+********************************************************************************************/
+
 DROP VIEW IF EXISTS [dbo].[v_accounts_per_customer_per_year];
 GO
 CREATE VIEW [v_accounts_per_customer_per_year]
