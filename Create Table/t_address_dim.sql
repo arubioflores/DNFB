ee5667eb47b92ae325b0836088f66a00dd16fa43
arubioflores <55721881+arubioflores@users.noreply.github.com>
2019-12-10 16:02:24 -0700

    Update t_address_dim.sql

--- Create Table/t_address_dim.sql
+++ Create Table/t_address_dim.sql
@@ -10,6 +10,7 @@ MODIFICATION LOG:
 Ver       Date         Author       Description
 -------   ----------   ----------   -----------------------------------------------------------------------------
 1.0       12/10/2019   ARUBIO      1. Built this table
+1.1       12/10/2019   ARUBIO      2. Added correct formatting
 
 
 
@@ -24,6 +25,9 @@ This code is covered by the GNU General Public License which guarantees end user
 the freedom to run, study, share, and modify the code. This license grants the recipients
 of the code the rights of the Free Software Definition. All derivative work can only be
 distributed under the same license terms.
+
+********************************************************************************************/
+
 USE [DFNB2];
 
 DROP TABLE IF EXISTS t_address_dim;
