1e5c97ae228b4e83820bbd2c3ff2f855e277156d
arubioflores <55721881+arubioflores@users.noreply.github.com>
2019-12-10 15:58:39 -0700

    Update t_address_dim.sql

--- Create Table/t_address_dim.sql
+++ Create Table/t_address_dim.sql
@@ -1,3 +1,29 @@
+/*****************************************************************************************************************
+NAME:    t_address_dim.sql
+PURPOSE: Create the t_address_dim.sql table
+
+SUPPORT: Alex Rubio 
+         rubioalejandro1114@gmail.com
+         aar43@ldsbc.edu
+
+MODIFICATION LOG:
+Ver       Date         Author       Description
+-------   ----------   ----------   -----------------------------------------------------------------------------
+1.0       12/10/2019   ARUBIO      1. Built this table
+
+
+
+RUNTIME: 
+1 min
+
+NOTES: 
+(...)
+
+LICENSE: 
+This code is covered by the GNU General Public License which guarantees end users
+the freedom to run, study, share, and modify the code. This license grants the recipients
+of the code the rights of the Free Software Definition. All derivative work can only be
+distributed under the same license terms.
 USE [DFNB2];
 
 DROP TABLE IF EXISTS t_address_dim;
